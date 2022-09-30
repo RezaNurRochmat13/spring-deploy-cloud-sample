5ab7863851738b55507736deaeacb50c13e4790c
Reja <rezanurrochmat@gmail.com>
2022-09-30 11:27:29 +0700

    add jpa maven

---
+++ src/main/java/com/fly/io/deploy/FlyIODeploy/entity/Person.java
@@ -0,0 +1,19 @@
+package com.fly.io.deploy.FlyIODeploy.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "persons")
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "address")
+    private String address;
+
+
+}
