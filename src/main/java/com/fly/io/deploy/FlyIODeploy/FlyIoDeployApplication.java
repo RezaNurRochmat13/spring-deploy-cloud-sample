11fbc7c65390bdc5837716ae6e29d36a8c862fc8
Reja <rezanurrochmat@gmail.com>
2022-09-30 11:11:12 +0700

    initial project

---
+++ src/main/java/com/fly/io/deploy/FlyIODeploy/FlyIoDeployApplication.java
@@ -0,0 +1,13 @@
+package com.fly.io.deploy.FlyIODeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyIoDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlyIoDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fly/io/deploy/FlyIODeploy/FlyIoDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fly.io.deploy.FlyIODeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyIoDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
